182a249ed8050746423d35c6d3541818b95d38f7
Mikatahara <fabercastell679@yahoo.com>
2020-03-27 08:59:34 +0900

    wavload

--- js/pianowavload.js
+++ js/pianowavload.js
@@ -4,22 +4,22 @@ window.addEventListener('load', function (){
 
 	mSoundName=new Array(17);
 
-	mSoundName[0]="http://haramikata.jougennotuki.com/wav/tbp/001_B0.wav" ;
-	mSoundName[1]="http://haramikata.jougennotuki.com/wav/tbp/002_C0.wav" ;
-	mSoundName[2]="http://haramikata.jougennotuki.com/wav/tbp/003_F0.wav" ;
-	mSoundName[3]="http://haramikata.jougennotuki.com/wav/tbp/004_A1.wav" ;
-	mSoundName[4]="http://haramikata.jougennotuki.com/wav/tbp/005_D1.wav" ;
-	mSoundName[5]="http://haramikata.jougennotuki.com/wav/tbp/006_Fs1.wav";
-	mSoundName[6]="http://haramikata.jougennotuki.com/wav/tbp/007_A2.wav" ;
-	mSoundName[7]="http://haramikata.jougennotuki.com/wav/tbp/008_Cs2.wav";
-	mSoundName[8]="http://haramikata.jougennotuki.com/wav/tbp/009_E2.wav" ;
-	mSoundName[9]="http://haramikata.jougennotuki.com/wav/tbp/010_Cs3.wav";
-	mSoundName[10]="http://haramikata.jougennotuki.com/wav/tbp/011_F3.wav" ;
-	mSoundName[11]="http://haramikata.jougennotuki.com/wav/tbp/012_Gs3.wav";
-	mSoundName[12]="http://haramikata.jougennotuki.com/wav/tbp/013_B4.wav" ;
-	mSoundName[13]="http://haramikata.jougennotuki.com/wav/tbp/014_D4.wav" ;
-	mSoundName[14]="http://haramikata.jougennotuki.com/wav/tbp/015_G4.wav" ;
-	mSoundName[15]="http://haramikata.jougennotuki.com/wav/tbp/016_D5.wav" ;
-	mSoundName[16]="http://haramikata.jougennotuki.com/wav/tbp/017_F5.wav" ;
+	mSoundName[0]="https://mikatahara.github.io/PianoMachine/wav/tbp/001_B0.wav" ;
+	mSoundName[1]="https://mikatahara.github.io/PianoMachine/wav/tbp/002_C0.wav" ;
+	mSoundName[2]="https://mikatahara.github.io/PianoMachine/wav/tbp/003_F0.wav" ;
+	mSoundName[3]="https://mikatahara.github.io/PianoMachine/wav/tbp/004_A1.wav" ;
+	mSoundName[4]="https://mikatahara.github.io/PianoMachine/wav/tbp/005_D1.wav" ;
+	mSoundName[5]="https://mikatahara.github.io/PianoMachine/wav/tbp/006_Fs1.wav";
+	mSoundName[6]="https://mikatahara.github.io/PianoMachine/wav/tbp/007_A2.wav" ;
+	mSoundName[7]="https://mikatahara.github.io/PianoMachine/wav/tbp/008_Cs2.wav";
+	mSoundName[8]="https://mikatahara.github.io/PianoMachine/wav/tbp/009_E2.wav" ;
+	mSoundName[9]="https://mikatahara.github.io/PianoMachine/wav/tbp/010_Cs3.wav";
+	mSoundName[10]="https://mikatahara.github.io/PianoMachine/wav/tbp/011_F3.wav" ;
+	mSoundName[11]="https://mikatahara.github.io/PianoMachine/wav/tbp/012_Gs3.wav";
+	mSoundName[12]="https://mikatahara.github.io/PianoMachine/wav/tbp/013_B4.wav" ;
+	mSoundName[13]="https://mikatahara.github.io/PianoMachine/wav/tbp/014_D4.wav" ;
+	mSoundName[14]="https://mikatahara.github.io/PianoMachine/wav/tbp/015_G4.wav" ;
+	mSoundName[15]="https://mikatahara.github.io/PianoMachine/wav/tbp/016_D5.wav" ;
+	mSoundName[16]="https://mikatahara.github.io/PianoMachine/wav/tbp/017_F5.wav" ;
 
 }, false);
